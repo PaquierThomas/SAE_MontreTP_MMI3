51ad6e611dd21202f7ff6d4098338c92ceb000ac
ThomasP <legends.legardus@gmail.com>
2024-01-14 15:55:41 +0100

    finaliser back panier

--- src/views/PanierView.vue
+++ src/views/PanierView.vue
@@ -1,4 +1,5 @@
 <template>
+  <Header />
   <div>
     <h1>Liste du Panier</h1>
     {{ user_id }}
@@ -10,15 +11,23 @@
         <p><strong>Texture Boitier:</strong> {{ article.TextureBoitier }}</p>
         <p><strong>Quantité:</strong> {{ article.Quantite }}</p>
         <p v-if="article.PrixTotal !== undefined">
-          <strong>Prix Total:</strong> {{ article.PrixTotal }} €
+          <strong>Prix de la montre:</strong> {{ article.PrixTotal }} €
         </p>
         <hr />
       </li>
     </ul>
-
     <p v-else>Aucun article dans le panier.</p>
+    <p v-if="totalPanier !== undefined"><strong>Total du panier:</strong> {{ totalPanier }} €</p>
+    <button @click="supprimerPanier">Supprimer le panier</button>
+    <button @click="acheterPanier">Valider le panier</button>
   </div>
+  <Footer />
 </template>
+
+<script setup>
+import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
+</script>
 
 <script>
 import axios from 'axios'
@@ -29,6 +38,19 @@ export default {
     return {
       panier: [],
       user_id: token
+    }
+  },
+  computed: {
+    // Propriété calculée pour calculer le total du panier
+    totalPanier() {
+      return this.panier.reduce((total, article) => {
+        // Assurez-vous que le prix total de l'article est défini
+        if (article.PrixTotal !== undefined) {
+          return total + article.PrixTotal
+        } else {
+          return total
+        }
+      }, 0)
     }
   },
   mounted() {
@@ -58,6 +80,52 @@ export default {
         }
       } catch (error) {
         console.error('Erreur lors de la récupération du panier', error)
+      }
+    },
+    async supprimerPanier() {
+      try {
+        const response = await fetch(`http://localhost:4000/api/panier/${this.user_id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token
+          }
+        })
+
+        if (response.ok) {
+          // Affiche une boîte de dialogue d'alerte si la suppression est réussie
+          alert('Panier supprimé avec succès.')
+
+          // Mettez à jour localement la liste des articles du panier, par exemple :
+          this.panier = []
+        } else {
+          console.error('Erreur lors de la suppression du panier')
+        }
+      } catch (error) {
+        console.error('Erreur lors de la suppression du panier', error)
+      }
+    },
+    async acheterPanier() {
+      try {
+        const response = await fetch(`http://localhost:4000/api/panier/${this.user_id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token
+          }
+        })
+
+        if (response.ok) {
+          // Affiche une boîte de dialogue d'alerte si la suppression est réussie
+          alert('Votre achat est un succès.')
+
+          // Mettez à jour localement la liste des articles du panier, par exemple :
+          this.panier = []
+        } else {
+          console.error('Erreur lors de la suppression du panier')
+        }
+      } catch (error) {
+        console.error('Erreur lors de la suppression du panier', error)
       }
     }
   }
